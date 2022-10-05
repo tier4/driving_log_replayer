175d2bd73bd553dc97cdc3ae1e1689990f8d38d1
Hayato Mizushima <hayato-m126@users.noreply.github.com>
2022-10-05 14:19:27 +0900

    chore: remap tf in bag

--- driving_log_replayer/launch/localization.launch.py
+++ driving_log_replayer/launch/localization.launch.py
@@ -24,7 +24,9 @@ def generate_launch_description():
         "localization", python_node=True
     )
     evaluator_shutdown = driving_log_replayer.launch_common.get_evaluator_shutdown(evaluator_node)
-    player = driving_log_replayer.launch_common.get_player()
+    player = driving_log_replayer.launch_common.get_player(
+        additional_argument=["--remap", "/tf:=/driving_log_replayer/unused_tf"]
+    )
     recorder = driving_log_replayer.launch_common.get_recorder(
         "localization.qos.yaml",
         [
