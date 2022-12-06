41b8930f05affb8d87621a8af613baccf25da3cf
Hayato Mizushima <hayato-m126@users.noreply.github.com>
2022-12-06 15:18:18 +0900

    feat: kill previous zombie process

--- driving_log_replayer_cli/simulation/generate.py
+++ driving_log_replayer_cli/simulation/generate.py
@@ -52,6 +52,12 @@ class TestScriptGenerator:
                     command = self.__parse_scenario(dataset_path)
                     if command is not None:
                         generated_cmd += command + "\n"
+                        # kill zombie ros2 process
+                        generated_cmd += "ps aux | grep ros2 | grep -v grep | awk '{ print \"kill -9\", $2 }' | sh"
+                        # kill rviz
+                        generated_cmd += "ps aux | grep rviz | grep -v grep | awk '{ print \"kill -9\", $2 }' | sh"
+                        # sleep 1 sec
+                        generated_cmd += "sleep 1"
             with open(self.script_path, "w") as f:
                 f.write(generated_cmd)
             return True
