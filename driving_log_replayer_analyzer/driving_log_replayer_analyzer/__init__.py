7265f4935dd66de59c42cad1aa819bcf818ac781
Hayato Mizushima <hayato-m126@users.noreply.github.com>
2022-12-26 16:04:16 +0900

    feat: create sub command

--- driving_log_replayer_analyzer/driving_log_replayer_analyzer/__init__.py
+++ driving_log_replayer_analyzer/driving_log_replayer_analyzer/__init__.py
@@ -11,3 +11,36 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+import importlib.metadata
+
+import click
+from driving_log_replayer_analyzer.analysis import analysis
+
+__version__ = importlib.metadata.version("driving_log_replayer_cli")
+
+CONTEXT_SETTINGS = {"help_option_names": ["-h", "--help"]}
+
+
+def main():
+    cmd.add_command(analysis)
+    cmd()
+
+
+def print_version(ctx, param, value):
+    if not value or ctx.resilient_parsing:
+        return
+    click.echo(__version__)
+    ctx.exit()
+
+
+@click.group(context_settings=CONTEXT_SETTINGS)
+@click.option(
+    "--version", "-v", is_flag=True, callback=print_version, expose_value=False, is_eager=True
+)
+def cmd():
+    """Command line tool to use driving_log_replayer.
+
+    https://github.com/tier4/driving_log_replayer
+    """
+    pass
