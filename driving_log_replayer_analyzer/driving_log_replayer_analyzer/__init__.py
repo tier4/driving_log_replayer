1d2735052e77d89e2944fcd4ac8a5d4ebd0f724d
Hayato Mizushima <hayato-m126@users.noreply.github.com>
2023-01-13 12:46:47 +0900

    fix: version

--- driving_log_replayer_analyzer/driving_log_replayer_analyzer/__init__.py
+++ driving_log_replayer_analyzer/driving_log_replayer_analyzer/__init__.py
@@ -16,7 +16,7 @@
 from importlib.metadata import version
 
 try:
-    __version__ = version(__package__)
+    __version__ = version("driving_log_replayer_cli")
 except Exception:
     __version__ = "0.0.0"
 
