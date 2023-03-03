f8926233a5fe55ec17fd4f7f26a40b0a8ef93da9
Hayato Mizushima <hayato-m126@users.noreply.github.com>
2023-03-03 10:25:34 +0900

    chore: comment out analyzer

--- driving_log_replayer/scripts/traffic_light_evaluator_node.py
+++ driving_log_replayer/scripts/traffic_light_evaluator_node.py
@@ -256,22 +256,19 @@ class TrafficLightEvaluator(Node):
                 self.__pickle_writer = PickleWriter(self.__pkl_path)
                 self.__pickle_writer.dump(self.__evaluator.frame_results)
                 self.get_final_result()
-                analyzer = PerceptionAnalyzer2D(self.__evaluator.evaluator_config)
-                analyzer.add(self.__evaluator.frame_results)
-                score_df, error_df = analyzer.analyze()
-                score_dict = {}
-                error_dict = {}
-                if score_df is not None:
-                    score_dict = score_df.to_dict()
-                if error_df is not None:
-                    error_dict = (
-                        error_df.groupby(level=0)
-                        .apply(lambda df: df.xs(df.name).to_dict())
-                        .to_dict()
-                    )
-                final_metrics = {"Score": score_dict, "Error": error_dict}
-                self.__result.add_final_metrics(final_metrics)
-                self.__result_writer.write(self.__result)
+                # このコールバックが無限ループする
+                # analyzer = PerceptionAnalyzer2D(self.__evaluator.evaluator_config)
+                # analyzer.add(self.__evaluator.frame_results)
+                # score_df, error_df = analyzer.analyze()
+                # score_dict = {}
+                # error_dict = {}
+                # if score_df is not None:
+                #     score_dict = score_df.to_dict()
+                # if error_df is not None:
+                #     error_dict = error_df.to_dict()
+                # final_metrics = {"Score": score_dict, "Error": error_dict}
+                # self.__result.add_final_metrics(final_metrics)
+                # self.__result_writer.write(self.__result)
                 self.__result_writer.close()
                 rclpy.shutdown()
 
