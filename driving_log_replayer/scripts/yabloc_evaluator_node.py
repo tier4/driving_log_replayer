c42fff5e4309d16aa2159085e3c8ec7895d4b217
kminoda <koji.minoda@tier4.jp>
2023-07-31 15:30:53 +0900

    minor fix
    Signed-off-by: kminoda <koji.minoda@tier4.jp>

--- driving_log_replayer/scripts/yabloc_evaluator_node.py
+++ driving_log_replayer/scripts/yabloc_evaluator_node.py
@@ -18,23 +18,18 @@
 import os
 from typing import Dict
 
-from autoware_adapi_v1_msgs.srv import InitializeLocalization
 from diagnostic_msgs.msg import DiagnosticArray
 from driving_log_replayer.node_common import set_initial_pose
 from driving_log_replayer.result import ResultBase
 from driving_log_replayer.result import ResultWriter
-import numpy as np
 import rclpy
-from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
-from rclpy.clock import ClockType
 from rclpy.executors import MultiThreadedExecutor
 from rclpy.node import Node
-from tier4_localization_msgs.srv import PoseWithCovarianceStamped
 import yaml
 
 
 class YabLocResult(ResultBase):
-    def __init__(self, condition: Dict):
+    def __init__(self):
         super().__init__()
         # availability
         self.__yabloc_availability_result = False
@@ -67,8 +62,6 @@ class YabLocEvaluator(Node):
         self.declare_parameter("scenario_path", "")
         self.declare_parameter("result_json_path", "")
 
-        self.__timer_group = MutuallyExclusiveCallbackGroup()
-
         scenario_path = os.path.expandvars(
             self.get_parameter("scenario_path").get_parameter_value().string_value
         )
@@ -79,29 +72,13 @@ class YabLocEvaluator(Node):
             self.get_parameter("result_json_path").get_parameter_value().string_value
         )
 
-        self.__condition = self.__scenario_yaml_obj["Evaluation"]["Conditions"]
-
-        self.__result = YabLocResult(self.__condition)
-
         self.__result_writer = ResultWriter(
-            self.__result_json_path, self.get_clock(), self.__condition
+            self.__result_json_path, self.get_clock(), Dict()
         )
 
         _ = set_initial_pose(
             self.__scenario_yaml_obj["Evaluation"]["InitialPose"]
         )
-
-        # service client
-        self.__initial_pose_client = self.create_client(
-            InitializeLocalization, "/api/localization/initialize"
-        )
-        self.__map_fit_client = self.create_client(
-            PoseWithCovarianceStamped, "/map/map_height_fitter/service"
-        )
-        while not self.__initial_pose_client.wait_for_service(timeout_sec=1.0):
-            self.get_logger().warning("initial pose service not available, waiting again...")
-        while not self.__map_fit_client.wait_for_service(timeout_sec=1.0):
-            self.get_logger().warning("map height fitter service not available, waiting again...")
 
         self.__sub_diagnostics = self.create_subscription(
             DiagnosticArray,
