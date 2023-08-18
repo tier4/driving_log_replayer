a34b6aae1138abf0391be99f18beacf5ade20eb4
Hayato Mizushima <hayato-m126@users.noreply.github.com>
2023-08-18 15:04:38 +0900

    refactor: apply pie rules (#212)

--- driving_log_replayer_cli/configuration/__init__.py
+++ driving_log_replayer_cli/configuration/__init__.py
@@ -9,7 +9,6 @@ CONTEXT_SETTINGS = {"help_option_names": ["-h", "--help"]}
 @click.group(context_settings=CONTEXT_SETTINGS)
 def configuration():
     """Command to setup driving_log_replayer config file."""
-    pass
 
 
 @configuration.command(context_settings=CONTEXT_SETTINGS)
