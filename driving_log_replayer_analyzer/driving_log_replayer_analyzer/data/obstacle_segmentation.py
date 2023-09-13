1c814a6163021fdefd4d6643822e26c79ec04b11
Hayato Mizushima <hayato-m126@users.noreply.github.com>
2023-09-13 12:22:03 +0900

    fix: function name

--- driving_log_replayer_analyzer/driving_log_replayer_analyzer/data/obstacle_segmentation.py
+++ driving_log_replayer_analyzer/driving_log_replayer_analyzer/data/obstacle_segmentation.py
@@ -262,11 +262,11 @@ class JsonlParser:
         with open(output_path, "w") as f:
             writer = csv.writer(f)
             # header
-            writer.write_row(["UUID", "PC_Dist", "PC_NumPoints"])
+            writer.writerow(["UUID", "PC_Dist", "PC_NumPoints"])
             points_list = self.get_pointcloud_points_per_uuid()
             for uuid_points in points_list:
                 for frame in uuid_points:
-                    writer.write_row([frame[2], frame[0], frame[1]])
+                    writer.writerow([frame[2], frame[0], frame[1]])
 
     def get_topic_rate(self) -> list:
         ret = []
