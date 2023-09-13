a458610ef7f0926a9648b45ebe269bde9cac5e7f
Hayato Mizushima <hayato-m126@users.noreply.github.com>
2023-09-13 15:02:51 +0900

    feat: add launch common test

---
+++ driving_log_replayer/test/unittest/test_launch_common.py
@@ -0,0 +1,19 @@
+# Copyright (c) 2023 TIER IV.inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from driving_log_replayer.launch_common import snake_to_pascal
+
+
+def test_snake_to_pascal() -> None:
+    assert snake_to_pascal("driving_log_replayer") == "DrivingLogReplayer"
