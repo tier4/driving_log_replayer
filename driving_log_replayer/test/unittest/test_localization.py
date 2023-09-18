6358b5718735cc0751d60cb3f06cb86e2f520a26
Hayato Mizushima <hayato-m126@users.noreply.github.com>
2023-09-18 17:01:51 +0900

    feat: add reliability test

--- driving_log_replayer/test/unittest/test_localization.py
+++ driving_log_replayer/test/unittest/test_localization.py
@@ -12,12 +12,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from collections.abc import Callable
 
+from builtin_interfaces.msg import Time
 from diagnostic_msgs.msg import DiagnosticArray
 from diagnostic_msgs.msg import DiagnosticStatus
 from diagnostic_msgs.msg import KeyValue
+import pytest
+from tier4_debug_msgs.msg import Float32Stamped
 
 from driving_log_replayer.localization import AvailabilityResult
+from driving_log_replayer.localization import ReliabilityResult
 
 
 def test_availability_success() -> None:
@@ -69,6 +74,94 @@ def test_availability_has_no_target_diag() -> None:
             "Result": "Fail",
             "Info": [
                 {"Reason": "diagnostics does not contain localization_topic_status"},
+            ],
+        },
+    }
+
+
+@pytest.fixture()
+def create_reliability_result() -> ReliabilityResult:
+    return ReliabilityResult(
+        condition={"Method": "NVTL", "AllowableLikelihood": 2.3, "NGCount": 10},
+        total=9,
+        ng_seq=9,
+        received_data=[1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9],
+    )
+
+
+def test_reliability_success(create_reliability_result: Callable) -> None:
+    result: ReliabilityResult = create_reliability_result
+    nvtl = Float32Stamped(stamp=Time(sec=123, nanosec=456), data=2.3)
+    # The function to create dict of map_to_baselink is checked in the evaluator test.
+    map_to_baselink = {}
+    tp = Float32Stamped(stamp=Time(sec=123, nanosec=123), data=2.3)
+    frame = result.set_frame(nvtl, map_to_baselink, tp)
+    assert result.success is True
+    assert (
+        result.summary
+        == "Reliability (Success): NVTL Sequential NG Count: 0 (Total Test: 10, Average: 1.58000, StdDev: 0.34293)"
+    )
+    assert result.received_data == [1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.3]
+    assert frame == {
+        "Ego": {"TransformStamped": {}},
+        "Reliability": {
+            "Result": "Success",
+            "Info": [
+                {
+                    "Value": {
+                        "stamp": {
+                            "sec": 123,
+                            "nanosec": 456,
+                        },
+                        "data": 2.3,
+                    },
+                    "Reference": {
+                        "stamp": {
+                            "sec": 123,
+                            "nanosec": 123,
+                        },
+                        "data": 2.3,
+                    },
+                },
+            ],
+        },
+    }
+
+
+def test_reliability_fail(create_reliability_result: Callable) -> None:
+    result: ReliabilityResult = create_reliability_result
+    nvtl = Float32Stamped(stamp=Time(sec=123, nanosec=456), data=2.0)
+    # The function to create dict of map_to_baselink is checked in the evaluator test.
+    map_to_baselink = {}
+    tp = Float32Stamped(stamp=Time(sec=123, nanosec=123), data=2.3)
+    frame = result.set_frame(nvtl, map_to_baselink, tp)
+    assert result.success is False
+    assert (
+        result.summary
+        == "Reliability (Fail): NVTL Sequential NG Count: 10 (Total Test: 10, Average: 1.55000, StdDev: 0.28723)"
+    )
+    assert result.received_data == [1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0]
+    assert frame == {
+        "Ego": {"TransformStamped": {}},
+        "Reliability": {
+            "Result": "Fail",
+            "Info": [
+                {
+                    "Value": {
+                        "stamp": {
+                            "sec": 123,
+                            "nanosec": 456,
+                        },
+                        "data": 2.0,
+                    },
+                    "Reference": {
+                        "stamp": {
+                            "sec": 123,
+                            "nanosec": 123,
+                        },
+                        "data": 2.3,
+                    },
+                },
             ],
         },
     }
