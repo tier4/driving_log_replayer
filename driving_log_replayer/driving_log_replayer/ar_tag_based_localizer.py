e3cca35ee10d5c256b5b92f2590573006f539841
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2023-09-25 18:15:01 +0900

    Added unit test
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

---
+++ driving_log_replayer/driving_log_replayer/ar_tag_based_localizer.py
@@ -0,0 +1,73 @@
+# Copyright (c) 2023 TIER IV.inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from dataclasses import dataclass
+from typing import ClassVar
+
+from diagnostic_msgs.msg import DiagnosticArray
+
+from driving_log_replayer.result import EvaluationItem
+from driving_log_replayer.result import ResultBase
+
+
+@dataclass
+class Availability(EvaluationItem):
+    name: ClassVar[str] = "ArTagBasedLocalizer Availability"
+    TARGET_DIAG_NAME: ClassVar[str] = "ar_tag_based_localizer_monitor: ar_tag_based_localizer_status"
+
+    def set_frame(self, msg: DiagnosticArray) -> dict:
+        include_target_status = False
+        for diag_status in msg.status:
+            if diag_status.name != Availability.TARGET_DIAG_NAME:
+                continue
+            include_target_status = True
+            values = {value.key: value.value for value in diag_status.values}
+            status_str = values.get("Availability", "Availability Key Not Found")  # avoid KeyError
+            self.success = status_str == "OK"
+            self.summary = f"{self.name} ({self.success_str()}): {status_str}"
+            break
+        if include_target_status:
+            return {
+                "Ego": {},
+                "Availability": {
+                    "Result": {"Total": self.success_str(), "Frame": self.success_str()},
+                    "Info": {},
+                },
+            }
+        return {
+            "Ego": {},
+            "Availability": {
+                "Result": {"Total": self.success_str(), "Frame": "Warn"},
+                "Info": {"Reason": "diagnostics does not contain ar_tag_based_localizer_status"},
+            },
+        }
+
+
+class ArTagBasedLocalizerResult(ResultBase):
+    def __init__(self) -> None:
+        super().__init__()
+        self.__availability = Availability()
+
+    def update(self) -> None:
+        summary_str = f"{self.__availability.summary}"
+        if self.__availability.success:
+            self._success = True
+            self._summary = f"Passed: {summary_str}"
+        else:
+            self._success = False
+            self._summary = f"Failed: {summary_str}"
+
+    def set_frame(self, msg: DiagnosticArray) -> None:
+        self._frame = self.__availability.set_frame(msg)
+        self.update()
--- driving_log_replayer/scripts/ar_tag_based_localizer_evaluator_node.py
+++ driving_log_replayer/scripts/ar_tag_based_localizer_evaluator_node.py
@@ -19,47 +19,7 @@ from diagnostic_msgs.msg import DiagnosticArray
 
 from driving_log_replayer.evaluator import DLREvaluator
 from driving_log_replayer.evaluator import evaluator_main
-from driving_log_replayer.result import ResultBase
-
-
-class ArTagBasedLocalizerResult(ResultBase):
-    def __init__(self) -> None:
-        super().__init__()
-        # availability
-        self.__ar_tag_based_localizer_availability_result = False
-        self.__ar_tag_based_localizer_availability_msg = "NotTested"
-
-    def update(self) -> None:
-        if self.__ar_tag_based_localizer_availability_result:
-            ar_tag_based_localizer_availability_summary = (
-                f"ArTagBasedLocalizer Availability (Passed): {self.__ar_tag_based_localizer_availability_msg}"
-            )
-        else:
-            ar_tag_based_localizer_availability_summary = (
-                f"ArTagBasedLocalizer Availability (Failed): {self.__ar_tag_based_localizer_availability_msg}"
-            )
-        summary_str = f"{ar_tag_based_localizer_availability_summary}"
-        if self.__ar_tag_based_localizer_availability_result:
-            self._success = True
-            self._summary = f"Passed: {summary_str}"
-        else:
-            self._success = False
-            self._summary = f"Failed: {summary_str}"
-
-    def set_frame(self, msg: DiagnosticArray) -> None:
-        for diag_status in msg.status:
-            out_frame = {"Ego": {}}
-            if diag_status.name != "ar_tag_based_localizer_monitor: ar_tag_based_localizer_status":
-                continue
-            values = {value.key: value.value for value in diag_status.values}
-            self.__ar_tag_based_localizer_availability_result = values["Availability"] == "OK"
-            self.__ar_tag_based_localizer_availability_msg = values["Availability"]
-            out_frame["Availability"] = {
-                "Result": "Success" if self.__ar_tag_based_localizer_availability_result else "Fail",
-                "Info": [],
-            }
-            self._frame = out_frame
-            self.update()
+from driving_log_replayer.ar_tag_based_localizer import ArTagBasedLocalizerResult
 
 
 class ArTagBasedLocalizerEvaluator(DLREvaluator):
@@ -86,7 +46,7 @@ class ArTagBasedLocalizerEvaluator(DLREvaluator):
 
 @evaluator_main
 def main() -> DLREvaluator:
-    return ArTagBasedLocalizerEvaluator("ar_tag_based_localizer_evaluator")
+    return ArTagBasedLocalizerEvaluator("yabloc_evaluator")
 
 
 if __name__ == "__main__":
