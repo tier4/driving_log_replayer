a472a78706fc317b47809c52d7f7a1411d2981e6
Hayato Mizushima <hayato-m126@users.noreply.github.com>
2023-10-06 13:24:26 +0900

    feat: move function

--- driving_log_replayer/driving_log_replayer/obstacle_segmentation.py
+++ driving_log_replayer/driving_log_replayer/obstacle_segmentation.py
@@ -12,9 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from pathlib import Path
 import sys
 from typing import ClassVar
 
+from ament_index_python.packages import get_package_share_directory
 import numpy as np
 from perception_eval.common.object import DynamicObject
 from perception_eval.evaluation.sensing.sensing_frame_config import SensingFrameConfig
@@ -27,12 +29,73 @@ from std_msgs.msg import ColorRGBA
 from std_msgs.msg import Header
 from visualization_msgs.msg import Marker
 from visualization_msgs.msg import MarkerArray
+import yaml
 
 import driving_log_replayer.perception_eval_conversions as eval_conversions
 from driving_log_replayer.result import EvaluationItem
 from driving_log_replayer.result import ResultBase
+from driving_log_replayer_analyzer.config.obstacle_segmentation import Config
+from driving_log_replayer_analyzer.config.obstacle_segmentation import load_config
+from driving_log_replayer_analyzer.data import DistType
+from driving_log_replayer_analyzer.data.obstacle_segmentation import fail_3_times_in_a_row
+from driving_log_replayer_analyzer.data.obstacle_segmentation import JsonlParser
+from driving_log_replayer_analyzer.plot import PlotBase
 from driving_log_replayer_msgs.msg import ObstacleSegmentationMarker
 from driving_log_replayer_msgs.msg import ObstacleSegmentationMarkerArray
+
+
+def default_config_path() -> Path:
+    return Path(
+        get_package_share_directory("driving_log_replayer_analyzer"),
+        "config",
+        "obstacle_segmentation.yaml",
+    )
+
+
+def update_config(config: Config, vehicle_model: str) -> Config:
+    vehicle_info_param = Path(
+        get_package_share_directory(vehicle_model + "_description"),
+        "config",
+        "vehicle_info.param.yaml",
+    )
+    with open(vehicle_info_param) as f:
+        yaml_obj = yaml.safe_load(f)
+
+    config.overhang_from_baselink = (
+        yaml_obj["/**"]["ros__parameters"]["front_overhang"]
+        + yaml_obj["/**"]["ros__parameters"]["wheel_base"]
+    )
+    return config
+
+
+def get_graph_data(
+    input_jsonl: Path,
+    vehicle_model: str,
+    output_dir: Path,
+    config_yaml: Path,
+    dist_type: DistType,
+) -> tuple[dict, dict]:
+    output_dir.mkdir(exist_ok=True)
+
+    # 設定ファイルのロード
+    config = load_config(config_yaml)
+    # ここにvehicle paramから更新するところ入れる
+    config = update_config(config, vehicle_model)
+
+    # Load result.jsonl
+    parser = JsonlParser(input_jsonl, config, dist_type)
+
+    detection_dist_plot = PlotBase()
+    detection_dist_plot.add_data(parser.get_bb_distance(), legend="1 Frame")
+
+    min3_data = fail_3_times_in_a_row(parser.get_bb_distance())
+    detection_dist_plot.add_data(min3_data, legend="3 Frames")
+
+    pointcloud_numpoints_plot = PlotBase()
+    for data in parser.get_pointcloud_points_per_uuid():
+        pointcloud_numpoints_plot.add_data(data, legend=data[0][2])
+
+    return detection_dist_plot.to_dict(), pointcloud_numpoints_plot.to_dict()
 
 
 def get_box_marker(
