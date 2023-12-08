49b4d23869b359b17923359ee4487198da15fbb9
Hayato Mizushima <hayato-m126@users.noreply.github.com>
2023-12-08 17:23:58 +0900

    feat: cli accepts arbitrary launch_arg

--- driving_log_replayer_cli/simulation/__init__.py
+++ driving_log_replayer_cli/simulation/__init__.py
@@ -10,13 +10,6 @@ from driving_log_replayer_cli.simulation.result import display_all
 from driving_log_replayer_cli.simulation.run import run as sim_run
 
 CONTEXT_SETTINGS = {"help_option_names": ["-h", "--help"]}
-PERCEPTION_MODES = (
-    "camera_lidar_radar_fusion",
-    "camera_lidar_fusion",
-    "lidar_radar_fusion",
-    "lidar",
-    "radar",
-)
 
 
 @click.group(context_settings=CONTEXT_SETTINGS)
@@ -26,40 +19,15 @@ def simulation() -> None:
 
 @simulation.command(context_settings=CONTEXT_SETTINGS)
 @click.option("--profile", "-p", type=str, default="default")
-@click.option("--rate", "-r", default=1.0)
-@click.option("--delay", "-d", default=10.0)
-@click.option("--no-json", is_flag=True, help="Do not convert jsonl files to json")
-@click.option(
-    "--perception-mode",
-    "-m",
-    type=click.Choice(PERCEPTION_MODES, case_sensitive=False),
-)
-@click.option("--override_record_topics", "-o", default=False, type=bool)
-@click.option("--override_topics_regex", "-x", default="/override_unused", type=str)
+@click.option("--launch_args", "-l", type=str, default="")
 def run(
     profile: str,
-    rate: float,
-    delay: float,
-    no_json: bool,  # noqa
-    override_record_topics: bool,  # noqa
-    override_topics_regex: str,
-    perception_mode: str | None = None,
+    launch_args: str,
 ) -> None:
     config: Config = load_config(profile)
-    output_dir_by_time = config.output_directory.joinpath(
-        datetime.datetime.now().strftime("%Y-%m%d-%H%M%S")  # noqa
-    )
-    print(output_dir_by_time)  # noqa
     sim_run(
-        config.data_directory,
-        output_dir_by_time,
-        config.autoware_path,
-        rate,
-        delay,
-        perception_mode,
-        override_record_topics,
-        override_topics_regex,
-        not no_json,
+        config,
+        launch_args,
     )
 
 
--- driving_log_replayer_cli/simulation/run.py
+++ driving_log_replayer_cli/simulation/run.py
@@ -1,12 +1,16 @@
+import datetime
 from pathlib import Path
 import subprocess
 import sys
 
 import termcolor
+import yaml
 
-from driving_log_replayer_cli.simulation.generate import TestScriptGenerator
+from driving_log_replayer_cli.core.config import Config
 from driving_log_replayer_cli.simulation.result import convert_all
 from driving_log_replayer_cli.simulation.result import display_all
+
+USE_T4_DATASET = ("perception", "obstacle_segmentation", "perception_2d", "traffic_light")
 
 
 def run_with_log(cmd: list, log_path: Path) -> None:
@@ -25,37 +29,110 @@ def run_with_log(cmd: list, log_path: Path) -> None:
 
 
 def run(
-    data_directory: Path,
-    output_directory: Path,
-    autoware_path: Path,
-    rate: float,
-    delay: float,
-    perception_mode: str,
-    override_record_topics: bool,  # noqa
-    override_topics_regex: str,
-    output_json: bool,  # noqa
+    config: Config,
+    launch_args: str,
 ) -> None:
-    termcolor.cprint("<< generating launch command >>", "green")
-    generator = TestScriptGenerator(
-        data_directory,
-        output_directory,
-        autoware_path,
-        rate,
-        delay,
-        perception_mode,
-        override_record_topics,
-        override_topics_regex,
+    output_dir_by_time = create_output_dir_by_time(config.output_directory)
+    for dataset_path in config.data_directory.glob("*"):
+        # open scenario file and make ros2 launch command
+        if not dataset_path.is_dir():
+            continue
+        launch_cmd: str | None = create_launch_cmd(dataset_path)
+        if launch_cmd is None:
+            continue
+
+        # create dir for output bag and result.jsonl
+        output_case = output_dir_by_time.joinpath(dataset_path.name)
+        output_case.mkdir()
+
+        # create bash command
+        bash_cmd = create_bash_cmd(config, launch_args, launch_cmd)
+
+        # save command as bash script
+        run_script = output_case.joinpath("run.bash")
+        with run_script.open("w") as f:
+            f.write(bash_cmd)
+
+        # run simulation
+        cmd = ["/bin/bash", run_script.as_posix()]
+        # tryない方がいいかも
+        try:
+            run_with_log(cmd, output_case.joinpath("console.log"))
+        except KeyboardInterrupt:
+            termcolor.cprint("Simulation execution canceled by Ctrl+C", "red")
+            break
+
+    # convert result file and display result
+    convert_all(output_dir_by_time)
+    display_all(output_dir_by_time)
+
+
+def create_output_dir_by_time(base_path: Path) -> Path:
+    output_dir_by_time = base_path.joinpath(
+        datetime.datetime.now().strftime("%Y-%m%d-%H%M%S")  # noqa
     )
-    is_executable = generator.run()
-    if not is_executable:
-        print("aborted.")  # noqa
-        return
-    cmd = ["/bin/bash", generator.script_path.as_posix()]
-    try:
-        run_with_log(cmd, output_directory.joinpath("console.log"))
-    except KeyboardInterrupt:
-        termcolor.cprint("Simulation execution canceled by Ctrl+C", "red")
-    if output_json:
-        convert_all(output_directory)
-    termcolor.cprint("<< show test result >>", "green")
-    display_all(output_directory)
+    output_dir_by_time.mkdir()
+    symlink_dst = base_path.joinpath("latest").as_posix()
+    update_symlink = ["ln", "-snf", output_dir_by_time.as_posix(), symlink_dst]
+    subprocess.run(update_symlink, check=False)
+    return output_dir_by_time
+
+
+def create_launch_cmd(dataset_path: Path) -> str | None:
+    scenario_file_path = dataset_path.joinpath("scenario.yaml")
+    if not scenario_file_path.exists():
+        scenario_file_path = dataset_path.joinpath("scenario.yml")
+        if not scenario_file_path.exists():
+            termcolor.cprint(
+                scenario_file_path.as_posix() + " does not exist.",
+                "red",
+            )
+            return None
+    with scenario_file_path.open("r") as scenario_file:
+        scenario_dict = yaml.safe_load(scenario_file)
+    if scenario_dict["Evaluation"]["UseCaseName"] in USE_T4_DATASET:
+        return cmd_use_t4_dataset(scenario_file_path)
+    return cmd_use_bag_only(scenario_file_path)
+
+
+def create_bash_cmd(launch_cmd: str, launch_args: str, config: Config) -> str:
+    bash_cmd = f"source {config.autoware_path.joinpath('install', 'setup.bash').as_posix()}\n"
+    bash_cmd += launch_cmd + extract_arg(launch_args) + "\n"
+    bash_cmd += clean_up_cmd()
+    return bash_cmd
+
+
+def clean_up_cmd() -> str:
+    # echo return value of ros2 launch (0: ok, others: ng)
+    # kill zombie ros2 process
+    # kill rviz
+    # sleep 1 sec
+    return """echo $?
+pgrep ros | awk \'{ print "kill -9 $(pgrep -P ", $1, ") > /dev/null 2>&1" }\' | sh
+pgrep ros | awk \'{ print "kill -9 ", $1, " > /dev/null 2>&1" }\' | sh
+pgrep rviz | awk \'{ print "kill -9 $(pgrep -P ", $1, ") > /dev/null 2>&1" }\' | sh
+pgrep rviz | awk \'{ print "kill -9 ", $1, " > /dev/null 2>&1" }\' | sh
+sleep 1"""
+
+
+def cmd_use_t4_dataset(scenario_file_path: Path) -> str:
+    return ""
+
+
+def cmd_use_bag_only(scenario_file_path: Path) -> str:
+    return ""
+
+
+def extract_arg(launch_args: str) -> str:
+    extract_launch_arg = ""
+    keys_values_list = launch_args.split(",")
+    for kv_str in keys_values_list:
+        if len(kv_str.split(":=")) != 2:  # noqa
+            # invalid argument
+            termcolor.cprint(
+                f"{kv_str} is ignored because it is invalid",
+                "red",
+            )
+        else:
+            extract_launch_arg += f" {kv_str}"
+    return extract_launch_arg
