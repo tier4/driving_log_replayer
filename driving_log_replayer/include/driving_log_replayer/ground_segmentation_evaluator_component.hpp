0a08ce87084757ee2df071658d47df500446e4f6
nanoshimarobot <shimada.toyozo.lf@tut.jp>
2024-01-04 14:05:13 +0900

    del test

--- driving_log_replayer/include/driving_log_replayer/ground_segmentation_evaluator_component.hpp
+++ driving_log_replayer/include/driving_log_replayer/ground_segmentation_evaluator_component.hpp
@@ -35,7 +35,5 @@ private:
 
   std::tuple<float, float, float, float> compute_metrics(
     const float TP, const float FP, const float TN, const float FN);
-
-  void test(void);
 };
 }  // namespace driving_log_replayer
