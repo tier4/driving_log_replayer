695703abe38e3e57e0d23976bc32fb0887dd6480
Hayato Mizushima <hayato-m126@users.noreply.github.com>
2024-03-01 18:39:36 +0900

    feat: update set frame

--- driving_log_replayer/driving_log_replayer/annotation_lees_perception.py
+++ driving_log_replayer/driving_log_replayer/annotation_lees_perception.py
@@ -13,12 +13,12 @@
 # limitations under the License.
 
 from dataclasses import dataclass
+from dataclasses import field
 from typing import Literal
 
 from diagnostic_msgs.msg import DiagnosticArray
 from diagnostic_msgs.msg import DiagnosticStatus
 from pydantic import BaseModel
-from pydantic import field_validator
 import simplejson as json
 
 from driving_log_replayer.result import EvaluationItem
@@ -26,21 +26,11 @@ from driving_log_replayer.result import ResultBase
 from driving_log_replayer.scenario import Scenario
 
 
-class Criteria(BaseModel):
+class Conditions(BaseModel):
     Threshold: dict
-
-    @field_validator("Threshold")
-    @classmethod
-    def create_default_threshold(cls) -> dict:
-        return {}
 
     def set_threshold_from_arg(self, arg: str) -> None:
         self.Threshold = json.loads(arg)
-
-
-class Conditions(BaseModel):
-    LaunchSensing: bool
-    Criteria: Criteria
 
 
 class Evaluation(BaseModel):
@@ -57,24 +47,62 @@ class AnnotationLessPerceptionScenario(Scenario):
 class Deviation(EvaluationItem):
     name: str = "AnnotationLessPerception Deviation"
     success: bool = True
+    received_data: dict = field(default_factory=dict)
+    # received_data = {lateral_deviation: {min: sum_min, max: sum_max, mean: sum_mean} ... }
 
     def set_frame(self, msg: DiagnosticArray) -> dict:
-        self.summary = "Passed"
-        rtn_dict = {"Ego": {}, "Result": {"Total": self.success_str(), "Frame": "Success"}}
+        self.total += 1
+        frame_success = True
+        info_dict = {}
+        metrics_dict = {}
         for diag_status in msg.status:
             diag_status: DiagnosticStatus
             values = {
                 value.key: float(value.value) for value in diag_status.values
             }  # min, max, mean
-            rtn_dict[diag_status.name] = values
-        return rtn_dict
+            self.add(diag_status.name, values)
+            info_dict[diag_status.name] = values
+            metrics_dict[diag_status.name], diag_success = self.calc_average_and_success(
+                diag_status.name,
+                self.condition.Threshold[diag_status.name],
+            )
+            frame_success = frame_success and diag_success
+        self.success = frame_success
+        self.summary = f"{self.name} ({self.success_str()})"
+        return {
+            "Ego": {},
+            "Deviation": {
+                "Result": {"Total": self.success_str(), "Frame": self.success_str()},
+                "Info": info_dict,
+                "Metrics": metrics_dict,
+            },
+        }
+
+    def add(self, key: str, values: dict) -> None:
+        if self.received_data.get(key) is None:
+            self.received_data[key] = {"min": 0.0, "max": 0.0, "mean": 0.0}  # initialize
+        self.received_data[key]["min"] += values["min"]
+        self.received_data[key]["max"] += values["max"]
+        self.received_data[key]["mean"] += values["mean"]
+
+    def calc_average_and_success(self, key: str, threshold: dict) -> tuple[dict, bool]:
+        if self.received_data.get(key) is None or self.total == 0:
+            return {"min": 0.0, "max": 0.0, "mean": 0.0}
+        a_min = self.received_data[key]["min"] / self.total
+        a_max = self.received_data[key]["max"] / self.total
+        a_mean = self.received_data[key]["mean"] / self.total
+        is_success = (
+            (a_min <= threshold["min"])
+            and (a_max <= threshold["max"])
+            and (a_mean <= threshold["mean"])
+        )
+        return {"min": a_min, "max": a_max, "mean": a_mean}, is_success
 
 
 class AnnotationLessPerceptionResult(ResultBase):
-    def __init__(self, condition) -> None:  # noqa
-        # condition is for future extensibility and code commonality
+    def __init__(self, condition: Conditions) -> None:
         super().__init__()
-        self.__deviation = Deviation()
+        self.__deviation = Deviation(condition=condition)
 
     def update(self) -> None:
         summary_str = f"{self.__deviation.summary}"
