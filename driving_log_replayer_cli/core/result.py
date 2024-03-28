4afecc200cc10a869a0a6f4ca5fe6bdbe2652e4b
Hayato Mizushima <hayato-m126@users.noreply.github.com>
2024-03-28 11:47:31 +0900

    refactor: cli change structure (#382)

--- driving_log_replayer_cli/core/result.py
+++ driving_log_replayer_cli/core/result.py
@@ -1,4 +1,3 @@
-import contextlib
 from pathlib import Path
 
 import simplejson as json
@@ -7,31 +6,43 @@ import termcolor
 
 def display(result_path: Path) -> None:
     print("--------------------------------------------------")  # noqa
-    result_json_dict = {}
-    with result_path.open() as jsonl_file:
-        last_line = jsonl_file.readlines()[-1]
-        with contextlib.suppress(json.JSONDecodeError):
-            result_json_dict = json.loads(last_line)
-    if result_json_dict:
+    last_result = load_last_result(result_path)
+    if last_result:
         # dict is not empty
-        if result_json_dict["Result"]["Success"]:
+        if last_result["Result"]["Success"]:
             result = "TestResult: Passed"
             color = "green"
         else:
             result = "TestResult: Failed"
             color = "red"
         termcolor.cprint(result, color)
-        termcolor.cprint(result_json_dict["Result"]["Summary"], color)
+        termcolor.cprint(last_result["Result"]["Summary"], color)
 
 
-def convert(result_path: Path) -> None:
+def load_last_result(result_path: Path) -> dict:
+    result_list = load_result(result_path)
+    if result_list == []:
+        return {}
+    return result_list[-1]
+
+
+def load_result(result_path: Path) -> list[dict]:
+    if not result_path.exists():
+        return []
+    with result_path.open() as jsonl_file:
+        try:
+            return [json.loads(line) for line in jsonl_file]
+        except json.JSONDecodeError:
+            return []
+
+
+def convert_to_json(result_path: Path) -> None:
     output_file_path = result_path.parent.joinpath("result.json")
     if output_file_path.exists():
         return
-    with result_path.open() as jsonl_file:
-        result_dict = [json.loads(line) for line in jsonl_file]
+    result_list = load_result(result_path)
     with output_file_path.open("w") as out_file:
-        json.dump(result_dict, out_file)
+        json.dump(result_list, out_file)
 
 
 def display_all(output_directory: Path) -> None:
@@ -44,4 +55,4 @@ def display_all(output_directory: Path) -> None:
 def convert_all(output_directory: Path) -> None:
     result_paths = output_directory.glob("**/result.jsonl")
     for result_path in result_paths:
-        convert(result_path)
+        convert_to_json(result_path)
