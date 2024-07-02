f7b84af574aba854b80a5c5f49337ab686b9a012
Hayato Mizushima <hayato-m126@users.noreply.github.com>
2024-07-02 16:47:03 +0900

    fix: migration script

--- scenario_migrator/scenario_migrator.py
+++ scenario_migrator/scenario_migrator.py
@@ -38,14 +38,26 @@ def convert(scenario_path: Path) -> None:
 
 
 def move_dataset(scenario_path: Path) -> None:
+    # ファイル移動
     scenario_root = scenario_path.parent
     bag_path = scenario_root.joinpath("input_bag")
     t4_dataset_path = scenario_root.joinpath("t4_dataset")
+    # bag 移動
     if bag_path.exists():
-        # bag 移動
         t4_dataset_path.mkdir()
         bag_path.rename(Path(t4_dataset_path, "input_bag"))
     # t4_datasetの移動
+    elif t4_dataset_path.exists():
+        scenario_file = scenario_path.open()
+        yaml_obj = yaml.safe_load(scenario_file)
+        for t4_dataset_dict in yaml_obj["Evaluation"]["Datasets"]:
+            t4_dataset_dict: dict
+            for dataset_name in t4_dataset_dict:
+                new_dataset_dir = scenario_root.joinpath(dataset_name)
+                new_dataset_dir.mkdir()
+                t4_dataset_path.joinpath(dataset_name).rename(new_dataset_dir)
+        # remove base dir
+        t4_dataset_path.rmdir()
 
 
 def main() -> None:
