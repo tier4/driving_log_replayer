3193aec7c2cf6b588421f23f49945de0203616d6
Hayato Mizushima <hayato-m126@users.noreply.github.com>
2024-07-29 23:46:45 +0900

    fix: check length

--- driving_log_replayer/scripts/obstacle_segmentation_evaluator_node.py
+++ driving_log_replayer/scripts/obstacle_segmentation_evaluator_node.py
@@ -303,7 +303,7 @@ class ObstacleSegmentationEvaluator(DLREvaluator):
                     self.__latest_stop_reasons.append(message_to_ordereddict(msg_reason))
 
     def diag_cb(self, msg: DiagnosticArray) -> None:
-        if not msg.status:
+        if len(msg.status) == 0:
             return
         diag_status: DiagnosticStatus = msg.status[0]
         if diag_status.name == TARGET_DIAG_NAME:
