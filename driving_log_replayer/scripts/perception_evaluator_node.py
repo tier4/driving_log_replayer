539cc8f4860109ce744bb868a01c711daaa5c33f
Grzegorz Głowacki <gglowacki@autonomous-systems.pl>
2024-09-25 12:12:10 +0000

    refactor: Dependency Injection

--- driving_log_replayer/scripts/perception_evaluator_node.py
+++ driving_log_replayer/scripts/perception_evaluator_node.py
@@ -16,6 +16,7 @@
 
 import logging
 from pathlib import Path
+from collections.abc import Callable
 
 from autoware_perception_msgs.msg import DetectedObject
 from autoware_perception_msgs.msg import DetectedObjects
@@ -47,8 +48,8 @@ import driving_log_replayer.perception_eval_conversions as eval_conversions
 
 
 class PerceptionEvaluator(DLREvaluator):
-    def __init__(self, name: str) -> None:
-        super().__init__(name, PerceptionScenario, PerceptionResult)
+    def __init__(self, name: str, scenario_class: Callable = PerceptionScenario, result_class: Callable = PerceptionResult) -> None:
+        super().__init__(name, scenario_class, result_class)
         self._scenario: PerceptionScenario
         self._result: PerceptionResult
 
