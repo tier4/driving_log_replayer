bbb55564daad7c642ed529c794cf69ffea5bb21d
Hayato Mizushima <hayato-m126@users.noreply.github.com>
2024-09-27 12:21:52 +0900

    feat: change topic name
    Signed-off-by: Hayato Mizushima <hayato-m126@users.noreply.github.com>

--- driving_log_replayer/scripts/traffic_light_evaluator_node.py
+++ driving_log_replayer/scripts/traffic_light_evaluator_node.py
@@ -113,7 +113,7 @@ class TrafficLightEvaluator(DLREvaluator):
         )
         self.__sub_traffic_signals = self.create_subscription(
             TrafficLightGroupArray,
-            "/perception/traffic_light_recognition/traffic_signals",
+            "/perception/traffic_light_recognition/internal/traffic_signals",
             self.traffic_signals_cb,
             1,
         )
