51d1765c9324ba0c4c3adc329147a59993257acf
Hayato Mizushima <hayato-m126@users.noreply.github.com>
2025-01-30 18:22:54 +0900

    chore: Confirmation that the pydantic model will properly convert to float even if an integer value is inserted from the scenario.
    Signed-off-by: Hayato Mizushima <hayato-m126@users.noreply.github.com>

--- driving_log_replayer/test/unittest/test_scenario.py
+++ driving_log_replayer/test/unittest/test_scenario.py
@@ -14,6 +14,8 @@
 
 from pydantic import BaseModel
 
+from driving_log_replayer.scenario import Position
+
 
 class SampleModel(BaseModel):
     version: str
@@ -24,3 +26,14 @@ def test_type_check_base_model() -> None:
     sample_model = SampleModel(version="1.0.0", name="s_model")
     assert type(sample_model) is not BaseModel
     assert isinstance(sample_model, BaseModel)
+
+
+def test_position_type() -> None:
+    position = Position(x=1, y=2, z=3)  # set int value
+    assert position.x == 1.0
+    assert position.y == 2.0  # noqa
+    assert position.z == 3.0  # noqa
+    assert position.x == 1  # This line does not raise a AssertionError
+    assert isinstance(position.x, float)
+    assert isinstance(position.y, float)
+    assert isinstance(position.z, float)
